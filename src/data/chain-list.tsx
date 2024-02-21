2c2b6f696029e087cc48d3c8b2301bce149d1074
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-02-21 16:03:56 +0700

    feat: add chainlist

---
+++ src/data/chain-list.tsx
@@ -0,0 +1,70 @@
+export const chainList = [
+  {
+    symbol: "USDT",
+    address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  },
+  {
+    symbol: "BNB",
+    address: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
+  },
+  {
+    symbol: "USDC",
+    address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  },
+  {
+    symbol: "stETH",
+    address: "0xae7ab96520de3a18e5e111b5eaab095312d7fe84",
+  },
+  {
+    symbol: "LINK",
+    address: "0x514910771af9ca656af840dff83e8264ecf986ca",
+  },
+  {
+    symbol: "DAI",
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  },
+  {
+    symbol: "UNI",
+    address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+  },
+  {
+    symbol: "AXL",
+    address: "0x467719ad09025fcc6cf6f8311755809d45a5e5f3",
+  },
+  {
+    symbol: "PEPE",
+    address: "0x6982508145454ce325ddbe47a25d4ec3d2311933",
+  },
+  {
+    symbol: "DYDX",
+    address: "0x92d6c1e31e14520e676a687f0a93788b716beff5",
+  },
+  {
+    symbol: "LDO",
+    address: "0x5a98fcbea516cf06857215779fd812ca3bef1b32",
+  },
+  {
+    symbol: "ARB",
+    address: "0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1",
+  },
+  {
+    symbol: "SHIB",
+    address: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+  },
+  {
+    symbol: "NEAR",
+    address: "0x85f17cf997934a597031b2e18a9ab6ebd4b9f6a4",
+  },
+  {
+    symbol: "INJ",
+    address: "0xe28b3B32B6c345A34Ff64674606124Dd5Aceca30",
+  },
+  {
+    symbol: "1INCH",
+    address: "0x111111111117dc0aa78b770fa6a738034120c302",
+  },
+  {
+    symbol: "FRAX",
+    address: "0x853d955acef822db058eb8505911ed77f175b99e",
+  },
+];
